990962c50aaeae505ee3aa1a3eaf7fb7ced4f8c7
Ivan Siles <silesivansalustiano@gmail.com>
2024-03-23 07:05:42 -0300

    1.0.6

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# React + Vite
+# ZuelFrend
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Multiplica y vencerás!
 
-Currently, two official plugins are available:
+![] ()
+[Ir a google]()
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+```
+Hola
+```
+```javascript
+const lol = {
+    
+}
+```
+
+### IMPORTANTE: Variables a configurar necesarias para el proyecto
+
+- VITE_API_KEY
+- VITE_AUTH_DOMAIN
+- VITE_PROJECT_ID
+- VITE_STORAGE_BUCKET
+- VITE_MESSAGING_SENDER_ID
+- VITE_APP_ID
+- VITE_MEASUREMENT_ID
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBpiemtanGPh3aVkpUokNSc4bVfGPxUYWE",
-  authDomain: "zuel-frend.firebaseapp.com",
-  projectId: "zuel-frend",
-  storageBucket: "zuel-frend.appspot.com",
-  messagingSenderId: "1049554229424",
-  appId: "1:1049554229424:web:eba5014edb72908fc84375",
-  measurementId: "G-G6CFDDBKTG"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
