c852330d02d949237e1d3c6c03ec3a37779655a5
Ivan Siles <silesivansalustiano@gmail.com>
2024-06-03 09:57:07 -0300

    1.0.7

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -56,8 +56,14 @@ router.post("/products", async (req, res) => {
 });
 router.get("/carts/:cid", async (req, res) => {
   const {cid} = req.params;
+  let completeCartResponse = await cartsModel.find({_id: cid}).lean()
   let cartResponse = await cartsCollection.getCartById(cid);
   const toSendObject = JSON.parse(JSON.stringify(cartResponse.products));
+  for (let i = 0; i < Object.values(toSendObject).length; i++) {
+    let myProducts = JSON.parse(JSON.stringify({...completeCartResponse[i]})).products[0]._id;
+    toSendObject[i] = {...toSendObject[i], ...myProducts}
+  }
+  console.log(toSendObject);
   res.render('cart', {toSendObject: toSendObject});
 });
 router.get("/realtimeproducts", (req, res) => {
